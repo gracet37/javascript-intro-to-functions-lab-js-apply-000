2fdb372aea6030fed19869cb6eb90bff6ccfbc5a
Grace Tan <gracetan37@gmail.com>
2019-02-28 07:49:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,20 +14,15 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
+
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
 }
-
-}
-function sayHiToGrandma(string) {
-  if (string.toUpperCase() === STRING) {
+else if (string.toUpperCase() === STRING) {
     return "YES INDEED!"
 }
-}
-
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") {
+else if (string === "I love you, Grandma.") {
     return "I love you, too."
 }
 }
